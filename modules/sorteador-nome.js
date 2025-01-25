ce87b935775a1ce17d5e7159a9229851947e9d04
Samuel Mori <samuelmori123@gmail.com>
2025-01-25 18:53:07 -0300

    feat: adicionada div que receberá as divs com os nomes que serão sorteados e adicionada função que cria a div dos nomes que serão sorteados

--- modules/sorteador-nome.js
+++ modules/sorteador-nome.js
@@ -1,8 +1,10 @@
 const quantidadeSortear = document.getElementById("quantidade-sortear");
 const textarea = document.getElementById("textarea");
 const sortear = document.getElementById("sortear");
+const adicionarNomes = document.getElementById("adicionarNomes");
 
 sortear.addEventListener("click", function() {
+    adicionarNomes.innerHTML = "";
     const quantidade = quantidadeSortear.value;
     const valorTextarea = textarea.value;
 
@@ -12,4 +14,18 @@ sortear.addEventListener("click", function() {
         .filter(item => item !== "");
 
     console.log(listaNomes);
+
+    listaNomes.forEach(nome => {
+        criarDivNome(nome);
+    });
 });
+
+function criarDivNome(nome){
+    const div = document.createElement("div");
+
+    const p = document.createElement("p");
+    p.innerText = nome;
+
+    div.appendChild(p);
+    adicionarNomes.appendChild(div);
+}
