7d5ae6e3b101dac81ab45f3ac7633ee47f1931e9
Samuel Mori <samuelmori123@gmail.com>
2025-01-28 19:49:06 -0300

    fix: problema de código no arquivo sorteador-nome.js onde havia um parêntes a mais impedindo que ele funcionasse

--- modules/sorteador-nome.js
+++ modules/sorteador-nome.js
@@ -4,7 +4,32 @@ export function pegarNumeroAleatorioListaNomes(listaNomes){
     return numeroAleatorio;
 }
 
-export function criarDivNome(nome){
+export function criarDivNome(nome) {
+    const listaNomes = valorTextarea
+        .split(",")
+        .map(item => item.trim())
+        .filter(item => item !== "");
+
+    console.log(listaNomes);
+
+    listaNomes.forEach(nome => {
+        criarDivNome(nome);
+    });
+
+    const numeroAletorio = pegarNumeroAleatorioListaNomes(listaNomes);
+
+    console.log(listaNomes[numeroAletorio]);
+
+    
+};
+
+function pegarNumeroAleatorioListaNomes(listaNomes){
+    const numeroAleatorio = Math.floor(Math.random() * listaNomes.length);
+
+    return numeroAleatorio;
+}
+
+function criarDivNome(nome){
     const div = document.createElement("div");
     div.classList.add("divNome");
 
